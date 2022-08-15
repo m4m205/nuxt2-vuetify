60da90b9053f8fb025fbd232f907aff7baa760d8
Mahmoud <m4m205@hotmail.com>
2022-08-15 11:36:49 +0200

    improve listComponent style

--- components/listComponent.vue
+++ components/listComponent.vue
@@ -1,17 +1,29 @@
 <template>
-  <v-alert
-    :border="isSelected ? 'left' : undefined"
-    colored-border
-    color="blue"
-    elevation="5"
-    class="list-card"
-    @click=""
-  >
-    <p>{{ listObj.name }}</p>
-    <v-btn icon @click.stop="$emit('delete-list', listObj.id)">
-      <v-icon color="red">mdi-delete</v-icon>
-    </v-btn>
-  </v-alert>
+  <v-hover v-slot="{ hover }">
+    <v-alert
+      :border="isSelected ? 'left' : undefined"
+      type="info"
+      text
+      color="primary"
+      :elevation="hover ? 10 : 4"
+      class="list-card"
+      icon="mdi-list-box"
+      prominent
+    >
+      <!-- <p>{{ listObj.name }}</p>
+      <v-btn icon @click.stop="$emit('delete-list', listObj.id)">
+        <v-icon color="red">mdi-delete</v-icon>
+      </v-btn> -->
+      <v-row align="center">
+        <v-col class="grow"> {{ listObj.name }} </v-col>
+        <v-col class="shrink">
+          <v-btn icon @click.stop="$emit('delete-list', listObj.id)">
+            <v-icon color="red">mdi-delete</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-alert>
+  </v-hover>
 </template>
 <script>
 export default {
