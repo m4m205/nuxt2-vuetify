4de67f9c0b2fd258d9468a07219533ed634d1f12
Mahmoud <m4m205@hotmail.com>
2022-08-15 12:59:09 +0200

    add audit trail

--- components/itemComponent.vue
+++ components/itemComponent.vue
@@ -1,24 +1,60 @@
 <template>
-  <v-card class="pa-2 mb-2" shaped>
-    <p>{{ itemObj.name }}</p>
-    <p>completed: {{ itemObj.completed }}</p>
+  <div>
+    <v-card class="pa-2 mb-2" shaped>
+      <p>{{ itemObj.name }}</p>
+      <p>completed: {{ itemObj.completed }}</p>
 
-    <v-btn icon @click.stop="">
-      <v-icon color="green">mdi-check-circle</v-icon>
-    </v-btn>
-    <v-btn icon @click.stop="$emit('delete-item', itemObj.id)">
-      <v-icon color="red">mdi-delete</v-icon>
-    </v-btn>
-    <v-btn icon @click.stop="">
-      <v-icon color="blue">mdi-information-outline</v-icon>
-    </v-btn>
-  </v-card>
+      <v-dialog v-model="dialog" max-width="290">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon @click.stop="" v-bind="attrs" v-on="on">
+            <v-icon color="blue">mdi-information-outline</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title class="text-h5"> Audit trail details </v-card-title>
+          <v-card-text>
+            <p>created at :{{ formatCreatedDate }}</p>
+            <p>updated at : {{ formatUpdatedDate }}</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="dialog = false">
+              Okay!
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-btn icon @click.stop="$emit('delete-item', itemObj.id)">
+        <v-icon color="red">mdi-delete</v-icon>
+      </v-btn>
+    </v-card>
+  </div>
 </template>
 <script>
 export default {
   props: {
-    "item-obj": Object,
+    itemObj: Object,
     isSelected: Boolean,
+  },
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  computed: {
+    formatCreatedDate: function () {
+      return this.itemObj.created_at.substring(
+        0,
+        this.itemObj.created_at.indexOf(".")
+      );
+    },
+    formatUpdatedDate: function () {
+      return this.itemObj.updated_at.substring(
+        0,
+        this.itemObj.updated_at.indexOf(".")
+      );
+    },
   },
 };
 </script>
